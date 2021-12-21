ee0c47b32535863bf176826f78ac08d648f9cfa5
Emese Sukei <emese.sukei@gmail.com>
2021-12-21 18:56:59 +0100

    setup.py updated

--- setup.py
+++ setup.py
@@ -5,18 +5,13 @@ See:
 https://packaging.python.org/guides/distributing-packages-using-setuptools/
 https://github.com/pypa/sampleproject
 """
-
-from pyhhmm import __version__
-import pathlib
 from setuptools import setup, find_packages
+import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
-long_description = (here / 'README.rst').read_text(encoding='utf-8')
-
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
+# long_description = (here / 'README.rst').read_text(encoding='utf-8')
 
 setup(
     # This is the name of your project. The first time you publish this
@@ -38,7 +33,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version=__version__,  # Required
+    version='2.0.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -53,7 +48,7 @@ setup(
     #
     # This field corresponds to the "Description" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-optional
-    long_description=long_description,  # Optional
+    # long_description=long_description,  # Optional
 
     # Denotes that our long_description is in Markdown; valid values are
     # text/plain, text/x-rst, and text/markdown
@@ -65,13 +60,13 @@ setup(
     #
     # This field corresponds to the "Description-Content-Type" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
-    long_description_content_type='text/x-rst',  # Optional (see note above)
+    # long_description_content_type='text/x-rst',  # Optional (see note above)
 
     # This should be a valid link to your project's main homepage.
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/fmorenopino/HeterogeneousHMM',  # Optional
+    # url='https://github.com/fmorenopino/HeterogeneousHMM',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
